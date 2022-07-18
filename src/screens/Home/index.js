e700302bf921775d63b160513616a32a1e177e17
Racchel <racchelvelasco@gmail.com>
2022-07-18 16:26:10 -0300

    Feature: add InfoSection component data

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,14 @@
 import { Layout } from '../Layout'
-import { HeroSection } from '../../components'
+import { HeroSection, InfoSection } from '../../components'
+import { Data } from '../../share'
 
 export const Home = () => {
    return (
       <Layout title='Home'>
          <HeroSection />
+         <InfoSection {...Data.infoSection.one} />
+         <InfoSection {...Data.infoSection.two} />
+         <InfoSection {...Data.infoSection.three} />
       </Layout>
    )
 }
---
+++ src/share/data/index.js
@@ -0,0 +1,58 @@
+import { assets } from '../../assets'
+
+const one = {
+   id: 'about',
+   light: false,
+   topLine: 'ainda não sei',
+   headLine: 'Sobre mim',
+   description: `is simply dummy text of the printing and typesetting industry. 
+   has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+   It has survived not only five centuries, but also the leap into electronic typesetting, remaining `,
+   buttonLabel: 'Vamo ver',
+   imgStart: false,
+   img: assets.images.woman,
+   alt: 'Imagem de mulher'
+}
+
+
+const two = {
+   id: 'projects',
+   light: true,
+   topLine: 'ainda não sei',
+   headLine: 'Meus projetos',
+   description: `is simply dummy text of the printing and typesetting industry. 
+   has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+   It has survived not only five centuries, but also the leap into electronic typesetting, remaining `,
+   buttonLabel: 'Vamo ver',
+   imgStart: true,
+   img: assets.images.projects,
+   alt: 'Imagem de mulher',
+}
+
+
+const three = {
+   id: 'blog',
+   light: false,
+   topLine: 'ainda não sei',
+   headLine: 'Blog',
+   description: `is simply dummy text of the printing and typesetting industry. 
+   has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+   It has survived not only five centuries, but also the leap into electronic typesetting, remaining `,
+   buttonLabel: 'Vamo ver',
+   imgStart: false,
+   img: assets.images.blog,
+   alt: 'Imagem de mulher',
+}
+
+const infoSection = {
+   one,
+   two,
+   three
+}
+
+export const Data = {
+   infoSection
+}
