0d50bd9bc291db8f1c41cc2a80a874d662cf66a4
Racchel <racchelvelasco@gmail.com>
2022-07-22 18:59:30 -0300

    Feature: create route and screen for first post article

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,8 +9,8 @@ export const AppRoutes = () => {
    return (
       <Routes>
          <Route path='/' element={<Home />} />
-         <Route path='/about' element={<p>Sobre</p>} />
-         <Route path='/projects' element={<p>Projetos</p>} />
+         {/* <Route path='/about' element={<p>Sobre</p>} />
+         <Route path='/projects' element={<p>Projetos</p>} /> */}
          <Route path='/blog' element={<Blog />} />
          <Route path='/blog/post/:id' element={<PostByID />} />
 
